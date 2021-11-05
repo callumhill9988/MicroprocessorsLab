affd864b4958d9ed16fdf51fb6188a62dd2ede94
ktk219 <ktk219@skbl-422-002.ph.ic.ac.uk>
2021-11-05 12:01:05 +0000

    Implemented 16 bit delay

--- main.s
+++ main.s
@@ -7,21 +7,45 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0		 ;Moves 0 to the working register   
-	movwf	TRISC, A	  ;Move the value 0 from the working register to TRISC-0 makes it an output  ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A          ;Move 0 from working register to 0x06
-	movlw	0xff			; all bits are inputs-0xff=8x1
-	movwf 	TRISD, A		; Port D TRIS Register
-	 		
-	movf	PORTD, W, A			;Puts value from port d into working memory-store it in access ram
-	
-	cpfsgt 	0x06, A ;compares value in 0x06 with with W,skip if f>w
-	bra 	loop		    ; Not yet finished goto start of loop again
+	movlw 	0x00		    ;Moves 0 to the working register   
+	movwf	TRISC, A	    ;Move the value 0 from the working register to TRISC-0 makes it an output  ; Port C all outputs
+	call read_d
+	call convert
 	goto 	0x0		    ; Re-run program from start
 
-	end	main
+read_d:
+	movwf	0x06, A		    ;Move 0 from working register to 0x06
+	movlw	0xff		    ; all bits are inputs-0xff=8x1
+	movwf 	TRISD, A	    ; Port D TRIS Register
+	 		
+	movf	PORTD, W, A
+	movwf 0x07, A		    ;Puts value from port d into working memory-store it in access ram
+	return 
+convert:
+        movff 0x07, 0x10, A		   ; moves input in D to 0x10, first 8 bits
+	movlw 0x00		    ; all zeros as last 8 bits
+	movwf 0x11, A		    ; move zeros to behind portD values
+	call Bigdelay
+	
+	movlw 0xff		    ; Turns all LEDS on
+        movwf PORTC
+	
+	call Bigdelay
+	
+	movlw 0x00		    ; Turns all LEDS off
+	movwf PORTC
+
+	return
+; implementation of the 16 bit delay
+Bigdelay: 
+	movlw 0x00 ; W = 0
+Dloop:	decf 0x11, f, A ; counter decrement
+	subwfb 0x10, f, A
+	bc Dloop
+	return 
+	
+end main       
+    
+        
+  
+    
