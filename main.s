bec9486efa15a5b49a147d847c1f2648378ef16f
ktk219 <ktk219@skbl-422-002.ph.ic.ac.uk>
2021-11-09 11:10:52 +0000

    implemented clock cycle for LED

--- main.s
+++ main.s
@@ -2,26 +2,55 @@
 
 	psect	code, abs
 main:
-	org	0x0		    ;tells assembler where to load instructions and data    
+	org	0x0		    ; tells assembler where to load instructions and data    
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0		 ;Moves 0 to the working register   
-	movwf	TRISC, A	  ;Move the value 0 from the working register to TRISC-0 makes it an output  ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A          ;Move 0 from working register to 0x06
-	movlw	0xff			; all bits are inputs-0xff=8x1
-	movwf 	TRISD, A		; Port D TRIS Register
-	 		
-	movf	PORTD, W, A			;Puts value from port d into working memory-store it in access ram
+	movlw 	0x0		    ; Moves 0 to the working register   
+	movwf	TRISD, A
+	movwf	TRISE, A	    ; Move the value 0 from the working register to TRISC-0 makes it an output  ; Port C all outputs
+	movlw   0x7		    ; Output LEDs
+	movwf   PORTE		    
 	
-	cpfsgt 	0x06, A ;compares value in 0x06 with with W,skip if f>w
-	bra 	loop		    ; Not yet finished goto start of loop again
+	call	clock		    ;
+	call	Bigdelay
+	call	Bigdelay
+	call	Bigdelay
+	call	Bigdelay
+	
+	movlw   0x0		    ; Output LEDs
+	movwf   PORTE		     
+
+	call	clock		    
+	call	Bigdelay
+	call	Bigdelay
+	call	Bigdelay
+	call	Bigdelay
+
 	goto 	0x0		    ; Re-run program from start
 
-	end	main
+clock:				    ; clock routine to register in memory
+	movlw	0x1		    ; Change clock from 0 to 1
+	movwf   PORTD
+	
+	movlw	0x0
+	 ;Change clock from 1 to 0
+	movwf   PORTD
+	
+	return
+    
+
+Bigdelay:
+	movlw	0xff
+	movwf	0x11, A
+	movwf	0x10, A
+	movlw	0x00
+Dloop:
+	decf 0x11, f, A
+
+	subwfb 0x10, f, A
+	bc Dloop
+	return
+	
+end	main
